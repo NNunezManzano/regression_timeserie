aebc5eae84ab09062e790779567eb2c19a13cd49
Nicolas Nuñez Manzano <nico.nm30@gmail.com>
2023-01-06 15:39:56 -0300

    multistep

--- src/multistep.py
+++ src/multistep.py
@@ -1,5 +1,25 @@
-"""
-   Design of multi step time serie
+""" This script will run if multistep was set on model.py """
 
-   It must be a class objet to be call by model.py
-"""
+import os
+
+import pandas as pd
+
+from feature_engineering import Feature_engineering as fe
+
+try:
+    os.chdir("../regression_timeserie")
+except:
+    os.chdir("../")
+
+
+col = {
+    "col1": ["volume", "guru", "guru", "guru", "guru", "guru"],
+    "col2": [1, 2, 3, 4, 5, 6],
+}
+
+df = pd.DataFrame.from_dict(col)
+
+df = fe.lags(df, 2, "col2")
+
+
+print(df)
