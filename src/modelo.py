104166bbad706a29fdef17aa4376a8c85c80ce50
Nicolas Nuñez Manzano <nico.nm30@gmail.com>
2023-01-26 14:21:17 -0300

    set params

--- src/modelo.py
+++ src/modelo.py
@@ -9,34 +9,71 @@ import lightgbm as lgb
 
 from data import Data_extract
 
+from feature_engineering import Feature_engineering
+
 from multistep import Multistep_reg
 
-# Parameters
-ticker = "SPY"
+# Set Parameters
+Params = {
+    "dex": {
+        "ticker": "SPY",
+        "start_date": "20200101",
+        "end_date": "20211231",
+    },
+    "fe": {
+        "lags": True,
+        "nlags": 60,
+        "lags_columns": ["Close"],
+        "trend": True,
+        "trends": [20, 40],
+        "trend_columns": ["Close"],
+        "stock": True,
+    },
+    "test_set": {"test_period": 30, "to_predict": "Close"},
+}
 
-start_date = "20200101"
-
-end_date = "20221231"
-
-test_period = 30
-
-
+# Initialazing etl classes
 # Data extraction
 dex = Data_extract()
 
-data = dex.get_data(ticker, start_date=start_date, end_date=end_date)
+# Feature engenieering
+
+fe = Feature_engineering()
+
+
+# get data from yfinance
+data = dex.get_data(
+    Params["dex"]["ticker"],
+    start_date=Params["dex"]["start_date"],
+    end_date=Params["dex"]["end_date"],
+)
+
 
 # train test split
-train = data.iloc[: len(data) - test_period]
-valid = data.iloc[len(data) - test_period :]
+train = data.iloc[: len(data) - Params["test_set"]["test_period"]]
+valid = data.iloc[len(data) - Params["test_set"]["test_period"] :]
 
-X_train = train.drop(columns=["Close", "Date"])
+# Train Test split
+X_train = train.drop(columns=[Params["test_set"]["Close"], "Date"])
 
-y_train = train.Close
+y_train = train[Params["test_set"]["Close"]]
 
-X_test = valid.drop(columns=["Close", "Date"])
+X_test = valid.drop(columns=[Params["test_set"]["Close"], "Date"])
 
-y_test = valid.Close
+y_test = valid[Params["test_set"]["Close"]]
+
+# Appling feature engineering to train dataset
+if Params["fe"]["lags"]:
+    for column in Params["fe"]["lags_columns"]:
+        X_train = fe.lags(X_train, n_lags=Params["fe"]["nlags"], column=column)
+
+if Params["fe"]["trend"]:
+    for column in Params["fe"]["trend_columns"]:
+        for trend in Params["fe"]["trends"]:
+            X_train = fe.trend(X_train, trend=trend, column=column)
+
+if Params["fe"]["stock"]:
+    X_train = fe.stocks_ts(X_train)
 
 # Regresor
 gbdt = lgb.LGBMRegressor(
@@ -58,5 +95,6 @@ msr.lightgb_ms(
     y_train=y_train,
     y_test=y_test,
     regresor=gbdt,
-    period=test_period,
+    period=Params["test_set"]["test_period"],
+    params=Params,
 )
